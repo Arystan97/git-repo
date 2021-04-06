d633bed69d85f8c34f05f62b402593800e2af99f
Arystan Atynov <Arystan.Atynov@gmail.com>
2021-04-06 19:20:35 +0600

    java project built with maven

---
+++ maven/Java/Java/src/main/java/tasks/Java collections/Main task/ElectronicDevices/Microwave.java
@@ -0,0 +1,8 @@
+package ElectronicDevices;
+
+public class Microwave extends ElectronicDevice
+{
+    public String toString(){
+        return "Energy Consumption of Microwave: " + getEnergyConsumption() + " Watts";
+    }
+}
---
+++ maven/Java/Java/src/main/java/tasks/Java collections/Main task/ElectronicDevices/TV.java
@@ -0,0 +1,8 @@
+package ElectronicDevices;
+
+public class TV extends ElectronicDevice
+{
+    public String toString(){
+        return "Energy Consumption of TV: " + getEnergyConsumption() + " Watts";
+    }
+}
